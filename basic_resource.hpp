8aa213780cad0a4012364fec82569438ebe72aaf
King-Yan Kwan <kykwan@linux.suse>
2016-10-23 10:51:40 +0800

    Use traits based design for resources to provide factory/disposal functions Test traits with custom FILE* wrapper

--- basic_resource.hpp
+++ basic_resource.hpp
@@ -1,19 +1,60 @@
 #ifndef _BASIC_RESOURCE_H_
 #define _BASIC_RESOURCE_H_
 
-namespace fm {
-template <typename T = int, T initial = -1, T error = initial>
-class basic_resource {
-  T t = initial;
+#include <algorithm>
 
-public:
+namespace fm {
+
+template <typename T> struct resource_traits;
+
+template <> struct resource_traits<int> {
+  constexpr static int initial = -1;
+
+  static int constructor() { return initial; }
+  static void destructor(int &t) { t = initial; }
 };
 
-template <typename T>
-class basic_resource<T *> : public basic_resource<T *, nullptr> {
-  T t = nullptr;
+template <typename T> struct resource_traits<T *> {
+  constexpr static T *initial = nullptr;
+
+  static T *constructor() { return initial; }
+  static void destructor(T *&t) { t = initial; }
+};
+
+template <typename T = int, typename Traits = resource_traits<T>>
+class basic_resource {
+  T t = Traits::initial;
 
 public:
+  basic_resource() = default;
+  basic_resource(basic_resource &&other) : t(other.t) {
+    other.t = Traits::initial;
+  };
+  ~basic_resource() {
+    if (t != Traits::initial)
+      Traits::destructor(t);
+  };
+
+  template <typename... Args>
+  basic_resource(Args &&... args) : t(Traits::constructor(args...)) {}
+
+  basic_resource(const basic_resource &) = delete;
+  basic_resource &operator=(const basic_resource &) = delete;
+  basic_resource &operator=(basic_resource &&) = delete;
+
+  static basic_resource steal(T &t) {
+    basic_resource resource;
+    std::swap(resource.t, t);
+    return resource;
+  }
+
+  operator T() { return t; }
+
+  T* release() {
+    T t1 = t;
+    t = Traits::initial;
+    return t1;
+  }
 };
 
 #include "basic_resource.ipp"
--- basic_resource_test.cpp
+++ basic_resource_test.cpp
@@ -1,8 +1,25 @@
 #include <cstdio>
 #include "basic_resource.hpp"
 
+namespace fm {
+struct FileTraits : public fm::resource_traits<std::FILE *> {
+  static std::FILE *constructor(const char *name, const char *mode) {
+    return std::fopen(name, mode);
+  }
+
+  static void destructor(std::FILE *f) { std::fclose(f); }
+};
+
+using file_resource = basic_resource<std::FILE *, FileTraits>;
+}
+
 int main() {
-  fm::basic_resource<FILE*> file_resource;
-  (void) file_resource;
+  auto file = std::fopen("non-existent", "r");
+  auto file_resource = fm::basic_resource<std::FILE *>::steal(file);
+  fclose(file_resource);
+
+  fm::basic_resource<std::FILE *> resource;
+
+  fm::file_resource other{"non-existent", "r"};
   return 0;
 }
