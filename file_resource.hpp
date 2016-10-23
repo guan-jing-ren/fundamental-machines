cbe154667f853c78859af8d29632a7b39e2cea67
King-Yan Kwan <kykwan@linux.suse>
2016-10-23 22:54:12 +0800

    Formatting

--- file_resource.hpp
+++ file_resource.hpp
@@ -3,16 +3,16 @@
 
 #include "basic_resource.hpp"
 
-    namespace fm {
-  struct FileTraits : public fm::resource_traits<std::FILE *> {
-    static std::FILE *constructor(const char *name, const char *mode) {
-      return std::fopen(name, mode);
-    }
+namespace fm {
+struct FileTraits : public fm::resource_traits<std::FILE *> {
+  static std::FILE *constructor(const char *name, const char *mode) {
+    return std::fopen(name, mode);
+  }
 
-    static void destructor(std::FILE *f) { std::fclose(f); }
-  };
+  static void destructor(std::FILE *f) { std::fclose(f); }
+};
 
-  using file_resource = basic_resource<std::FILE *, FileTraits>;
+using file_resource = basic_resource<std::FILE *, FileTraits>;
 }
 
 namespace std {
