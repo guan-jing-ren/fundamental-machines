d4ffe144ab76208cc9051d00cdb73ea94e6eb196
King-Yan Kwan <kykwan@linux.suse>
2016-10-24 00:15:52 +0800

    Figures out register size Checks register size is power of two

--- basic_register.hpp
+++ basic_register.hpp
@@ -2,11 +2,37 @@
 #define _BASIC_REGISTER_H_
 
 #include <type_traits>
+#include <limits>
+#include <utility>
 
 namespace fm {
+namespace detail {
+template <std::size_t... fields>
+constexpr auto register_size(std::integer_sequence<std::size_t, fields...>) {
+  std::size_t size = 0;
+  for (auto f : {fields...})
+    size += f;
+  return size;
+}
+
+constexpr auto power_of_two(std::size_t s) {
+  if (s == 1)
+    return true;
+  if (s % 2 == 1)
+    return false;
+  return power_of_two(s / 2);
+}
+}
+
 template <typename E, std::size_t... fields> struct basic_register {
   static_assert(std::is_enum<E>::value,
                 "Field accessors must be enumerated type");
+  static_assert(detail::power_of_two(detail::register_size(
+                    std::integer_sequence<std::size_t, fields...>{})),
+                "Total register size not a power of two");
+  static_assert(std::numeric_limits<std::underlying_type_t<E>>::max() >=
+                    sizeof...(fields),
+                "Field accessor is not large enough to address all fields");
 };
 }
 
--- basic_register_test.cpp
+++ basic_register_test.cpp
@@ -5,7 +5,7 @@ enum class RegField {
 };
 
 int main() {
-  fm::basic_register<RegField, 2, 5, 4, 7> reg;
+  fm::basic_register<RegField, 2, 5, 4, 7, 32 - 18> reg;
   (void)reg;
   return 0;
 }
