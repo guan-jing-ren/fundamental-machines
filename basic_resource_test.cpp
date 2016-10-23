c8402a8caa36b214d4443367cd295f71b668dd1e
King-Yan Kwan <kykwan@linux.suse>
2016-10-23 11:09:42 +0800

    Add checks to make sure resource was stolen

--- basic_resource_test.cpp
+++ basic_resource_test.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <stdexcept>
 #include "basic_resource.hpp"
 
 namespace fm {
@@ -14,9 +15,10 @@ using file_resource = basic_resource<std::FILE *, FileTraits>;
 }
 
 int main() {
-  auto file = std::fopen("non-existent", "r");
-
   fm::file_resource other{"non-existent", "r"};
+  auto file = std::fopen("non-existent-steal", "w");
+  if(!file) throw std::runtime_error{"File did not open properly"};
   auto other_steal = fm::file_resource::steal(file);
+  if(file) throw std::runtime_error{"File was not stolen"};
   return 0;
 }
