3b3b4908de5c2a556f4901083cd4d639a9581e2a
King-Yan Kwan <kykwan@linux.suse>
2016-10-23 20:42:48 +0800

    Add file resource specific versions of some FILE* functions

--- basic_resource_test.cpp
+++ basic_resource_test.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <type_traits>
 #include "test_utils.hpp"
 #include "basic_resource.hpp"
 
@@ -18,6 +19,78 @@ namespace std {
 namespace C {
 struct FILE : public fm::file_resource {
   using fm::file_resource::file_resource;
+
+  FILE &reopen(const char *mode) {
+    t = std::freopen(nullptr, mode, *this);
+    return *this;
+  }
+
+  FILE &reopen(const char *name, const char *mode) {
+    t = std::freopen(name, mode, *this);
+    return *this;
+  }
+
+  FILE &flush() {
+    std::fflush(t);
+    return *this;
+  }
+
+  FILE &wide(bool wide) {
+    std::fwide(*this, wide);
+    return *this;
+  }
+
+  FILE &setbuf(char *buffer) {
+    std::setbuf(*this, buffer);
+    return *this;
+  }
+
+  template <std::size_t N> FILE &setbuf(char (&buffer)[N]) {
+    static_assert(N <= BUFSIZ, "Provided buffer is not within limits");
+    std::setbuf(*this, buffer);
+    return *this;
+  }
+
+  template <int mode> FILE &setvbuf(char *buffer, std::size_t size) {
+    static_assert(mode == _IOFBF || mode == _IOLBF || mode == _IONBF,
+                  "Provided buffering mode is invalid, choose one of _IOFBF, "
+                  "_IOLBF, _IONBF");
+    std::setvbuf(*this, buffer, mode, size);
+    return *this;
+  }
+
+  template <int mode, std::size_t N> FILE &setvbuf(char (&buffer)[N]) {
+    static_assert(mode == _IOFBF || mode == _IOLBF || mode == _IONBF,
+                  "Provided buffering mode is invalid, choose one of _IOFBF, "
+                  "_IOLBF, _IONBF");
+    static_assert(N <= BUFSIZ, "Provided buffer is not within limits");
+    std::setvbuf(*this, buffer, mode, N);
+    return *this;
+  }
+
+  template <typename T> auto read(T *buffer, std::size_t count) {
+    static_assert(std::is_trivially_copyable<T>{},
+                  "Buffer type is not trivially copyable");
+    return std::fread(buffer, sizeof(T), count, *this);
+  }
+
+  template <typename T, std::size_t N> auto read(T (&buffer)[N]) {
+    static_assert(std::is_trivially_copyable<T>{},
+                  "Buffer type is not trivially copyable");
+    return std::fread(buffer, sizeof(T), N, *this);
+  }
+
+  template <typename T> auto write(T *buffer, std::size_t count) {
+    static_assert(std::is_trivially_copyable<T>{},
+                  "Buffer type is not trivially copyable");
+    return std::fwrite(buffer, sizeof(T), count, *this);
+  }
+
+  template <typename T, std::size_t N> auto write(T (&buffer)[N]) {
+    static_assert(std::is_trivially_copyable<T>{},
+                  "Buffer type is not trivially copyable");
+    return std::fwrite(buffer, sizeof(T), N, *this);
+  }
 };
 }
 }
