f9a1771d24d1dc3898c6505110b4344c983ad96a
King-Yan Kwan <kykwan@linux.suse>
2016-10-24 09:34:47 +0800

    Decide underlying representation depending on total field size

--- basic_register.hpp
+++ basic_register.hpp
@@ -3,6 +3,7 @@
 
 #include <type_traits>
 #include <limits>
+#include <cstdint>
 #include <utility>
 
 namespace fm {
@@ -22,17 +23,36 @@ constexpr auto power_of_two(std::size_t s) {
     return false;
   return power_of_two(s / 2);
 }
+
+template <std::size_t N> struct unit { using type = void; };
+template <> struct unit<8> { using type = std::uint8_t; };
+template <> struct unit<16> { using type = std::uint16_t; };
+template <> struct unit<32> { using type = std::uint32_t; };
+template <> struct unit<64> { using type = std::uint64_t; };
+template <std::size_t N> using unit_t = typename unit<N>::type;
 }
 
 template <typename E, std::size_t... fields> struct basic_register {
-  static_assert(std::is_enum<E>::value,
+  constexpr static std::size_t size =
+      detail::register_size(std::integer_sequence<std::size_t, fields...>{});
+  using field_accessor_type = std::remove_pointer_t<E>;
+  using value_type = detail::unit_t<size>;
+  using type =
+      std::conditional_t<!std::is_pointer<E>::value, volatile value_type,
+                         volatile value_type *const volatile>;
+
+  static_assert(std::is_enum<field_accessor_type>::value,
                 "Field accessors must be enumerated type");
-  static_assert(detail::power_of_two(detail::register_size(
-                    std::integer_sequence<std::size_t, fields...>{})),
+  static_assert(detail::power_of_two(size),
                 "Total register size not a power of two");
-  static_assert(std::numeric_limits<std::underlying_type_t<E>>::max() >=
-                    sizeof...(fields),
-                "Field accessor is not large enough to address all fields");
+  static_assert(
+      std::numeric_limits<std::underlying_type_t<field_accessor_type>>::max() >=
+          sizeof...(fields),
+      "Field accessor is not large enough to address all fields");
+
+  type r;
+
+  template <typename T> basic_register(T r) : r(reinterpret_cast<type>(r)) {}
 };
 }
 
--- basic_register_test.cpp
+++ basic_register_test.cpp
@@ -5,7 +5,7 @@ enum class RegField {
 };
 
 int main() {
-  fm::basic_register<RegField, 2, 5, 4, 7, 32 - 18> reg;
+  fm::basic_register<RegField *, 2, 5, 4, 7, 32 - 18> reg = 0x8843229;
   (void)reg;
   return 0;
 }
