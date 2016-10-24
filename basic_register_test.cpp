896943f006d575673456c7e318397c438d487e5e
King-Yan Kwan <kykwan@linux.suse>
2016-10-24 22:04:58 +0800

    Enable test for pretend memory mapped 'register'

--- basic_register_test.cpp
+++ basic_register_test.cpp
@@ -4,14 +4,16 @@
 
 enum class RegField { FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH };
 
+fm::detail::unit_t<32> r;
 int main() {
-  fm::basic_register<RegField *, 2, 5, 4, 7, 32 - 18> reg = 0x8843229;
-  //   reg.set<RegField::FIRST>(1);
-  (void)reg;
+  fm::basic_register<RegField *, 2, 5, 4, 7, 32 - 18> reg = &r;
+  reg.set<RegField::SECOND>(63);
+  reg.set<RegField::FIFTH>(7);
   fm::basic_register<RegField, 2, 5, 4, 7, 32 - 18> reg2 = 0;
   reg2.set<RegField::SECOND>(63);
   reg2.set<RegField::FIFTH>(7);
 
+  std::cout << std::bitset<32>{*reg.r}.to_string() << std::endl;
   std::cout << std::bitset<32>{reg2.r}.to_string() << std::endl;
   return 0;
 }
