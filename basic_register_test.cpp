2818f0e29c9b07cabbe34b2e90fc96675e66d855
King-Yan Kwan <kykwan@linux.suse>
2016-10-26 08:07:08 +0800

    Test typed register

--- basic_register_test.cpp
+++ basic_register_test.cpp
@@ -39,11 +39,35 @@ int main() {
   fm::basic_typed_register<fm::hardware_register, int, int, int, int,
                            int>::typed_register<RegField, 2, 5, 4, 7, 32 - 18>
       reg3 = 0;
-  (void)reg3;
   fm::basic_typed_register<fm::memory_mapped_register, int, int, int, int,
                            int>::typed_register<RegField, 2, 5, 4, 7, 32 - 18>
       reg4 = &r;
-  (void)reg4;
+
+  reg3.set<RegField::SECOND>(63);
+  reg3.set<RegField::FIFTH>(7);
+  reg4.set<RegField::SECOND>(63);
+  reg4.set<RegField::FIFTH>(7);
+
+  if (r != 0b00000000000111000000000001111100)
+    throw fm::test_error{"Memory mapped register test failed"};
+  if (reg3.r != 0b00000000000111000000000001111100)
+    throw fm::test_error{"Value register test failed"};
+  if (reg4.get<RegField::SECOND>() != 0b11111)
+    throw fm::test_error{"Field retrieval incorrect value"};
+  if (reg4.get<RegField::FIFTH>() != 0b111)
+    throw fm::test_error{"Field retrieval incorrect value"};
+  if (reg3.get<RegField::SECOND>() != 0b11111)
+    throw fm::test_error{"Field retrieval incorrect value"};
+  if (reg3.get<RegField::FIFTH>() != 0b111)
+    throw fm::test_error{"Field retrieval incorrect value"};
+
+  reg4.set<RegField::THIRD>(13);
+  reg3.set<RegField::THIRD>(13);
+
+  if (!reg4.test<RegField::THIRD>(0b1101))
+    throw fm::test_error{"Field test failed"};
+  if (!reg3.test<RegField::THIRD>(0b1101))
+    throw fm::test_error{"Field test failed"};
 
   return 0;
 }
