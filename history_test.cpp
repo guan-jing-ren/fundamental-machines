4b23b18c2c17622573de9c9c09d0aa00fc3dbf44
guan-jing-ren <kwan.king.yan@gmail.com>
2017-11-02 16:55:26 +1100

    Update history_test.cpp
    Make delta const correct and set it through constructor.
    Use use_count instead of deprecated unique.
    Attempt to collapse a modify if the modification field and value is the same.

--- history_test.cpp
+++ history_test.cpp
@@ -11,10 +11,12 @@ class History : public enable_shared_from_this<History<T, U...>> {
   friend int main();
   struct private_constructor_tag {};
   variant<shared_ptr<const History>, shared_ptr<const T>> base;
-  optional<variant<pair<U T::*, U>...>> delta;
+  const optional<variant<pair<U T::*, U>...>> delta;
 
 public:
-  History(shared_ptr<const History> h, private_constructor_tag) : base(h) {}
+  History(shared_ptr<const History> &&h, variant<pair<U T::*, U>...> &&d,
+          private_constructor_tag)
+      : base(h), delta(d) {}
 
   template <typename... V>
   History(private_constructor_tag, V &&... v)
@@ -22,7 +24,7 @@ public:
 
   ~History() {
     auto *dustbin = get_if<shared_ptr<const History>>(&base);
-    while (dustbin && dustbin->unique()) {
+    while (dustbin && dustbin->use_count() == 1) {
       auto previous = move(*dustbin);
       base = move(previous->base);
       dustbin = get_if<shared_ptr<const History>>(&base);
@@ -35,16 +37,18 @@ public:
   };
 
   template <typename V, typename W> auto modify(V T::*f, W &&w) const {
-    auto history = make_shared<History>(this->shared_from_this(),
-                                        private_constructor_tag{});
-    history->delta = make_pair(f, forward<W>(w));
-    return history;
+    const auto *d = delta ? get_if<pair<V T::*, V>>(&*delta) : nullptr;
+    if (d && *d == pair<V T::*, V>(f, w))
+      return this->shared_from_this();
+    return make_shared<const History>(this->shared_from_this(),
+                                      make_pair(f, forward<W>(w)),
+                                      private_constructor_tag{});
   }
 
   template <typename V> const V &at(V T::*f) const {
     const shared_ptr<const T> *principle = nullptr;
     const History *past = this;
-    auto *field = delta ? get_if<pair<V T::*, V>>(&*delta) : nullptr;
+    const auto *field = delta ? get_if<pair<V T::*, V>>(&*delta) : nullptr;
 
     while (!principle && (!field || field->first != f)) {
       field = nullptr;
@@ -75,6 +79,12 @@ public:
       return decltype(*principle){};
     return *principle;
   }
+
+  // Function: checkpoint
+  // Function: rebase
+  // Function: is ancestor
+  // Function: memory use
+  // Function: revisions
 };
 
 struct Object {
@@ -82,6 +92,20 @@ struct Object {
   string name = "Object 1";
   long _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, A, B, C, D, E, F;
 };
+
+// Functional handling of containers and get/set properties
+
+// Specialize/SFINAE History for entire values:
+// eg - history of numbers
+//    - history of optional
+//    - history of variant
+// when typename... U is empty?
+
+// Optimization cases
+// - no-change modification collapse
+// - field's last consecutive modification collapse
+//   - or subhistory for a field
+// vector<variant<pair<U T::*, U>...>> fields to allow batch modification
 
 int main() {
   auto object = History<Object, int, string, long>::create();
@@ -93,7 +117,7 @@ int main() {
       object->modify(&Object::field, 98)->modify(&Object::name, "Object 2");
 
   {
-    auto branch = object;
+    auto branch = object->back();
     for (auto i = 0; i < 10'000'000; ++i)
       branch = branch->modify(&Object::F, i);
 
@@ -101,6 +125,23 @@ int main() {
     //   cout << "Branch::F: " << branch->at(&Object::F) << '\n';
     //   branch = branch->back();
     // }
+  }
+
+  {
+    auto branch = object->back(); //->modify(&Object::F, 314159265);
+    for (auto i = 0; i < 10; ++i)
+      branch = branch->modify(&Object::F, i);
+
+    cout << "T0  use count: " << old.use_count() << '\n';
+    cout << "T-0 use count: " << object.use_count() << '\n';
+    cout << "T-1 use count: " << object->back().use_count() << '\n';
+
+    while (branch) {
+      cout << "Branch::F: " << branch->at(&Object::F) << ' '
+           << branch->at(&Object::field) << ' ' << branch->at(&Object::name)
+           << "; use count: " << branch.use_count() << '\n';
+      branch = branch->back();
+    }
   }
 
   cout << "T-0  object: " << object->at(&Object::field) << ' '
